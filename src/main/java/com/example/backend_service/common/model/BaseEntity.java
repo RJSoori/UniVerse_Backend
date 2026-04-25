d4d0165c11ab3ab7e35d51f4bcf8e7e729c4da74
Nisala Artigala <nisala.artigala@gmail.com>
2026-04-25 17:14:31 +0530

    dd GPA and Money Manager backend integration

--- src/main/java/com/example/backend_service/common/model/BaseEntity.java
+++ src/main/java/com/example/backend_service/common/model/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.example.backend_service.gpacalculator.model;
+package com.example.backend_service.common.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
--- src/main/java/com/example/backend_service/moneymanager/model/TransactionRecord.java
+++ src/main/java/com/example/backend_service/moneymanager/model/TransactionRecord.java
@@ -1,5 +1,6 @@
 package com.example.backend_service.moneymanager.model;
 
+import com.example.backend_service.common.model.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
