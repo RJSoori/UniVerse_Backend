0c72f1309902b3f9eb72082bea37244eb6412186
Nisala Artigala <nisala.artigala@gmail.com>
2026-05-12 04:52:56 +0530

    modification of unused code

--- src/main/java/com/example/backend_service/moneymanager/repository/RecurringExpenseRepository.java
+++ src/main/java/com/example/backend_service/moneymanager/repository/RecurringExpenseRepository.java
@@ -4,11 +4,8 @@ import com.example.backend_service.moneymanager.model.RecurringExpense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RecurringExpenseRepository extends JpaRepository<RecurringExpense, String> {
-    List<RecurringExpense> findByWalletId(String walletId);
     List<RecurringExpense> findByStudentId(Long studentId);
-    Optional<RecurringExpense> findByIdAndStudentId(String id, Long studentId);
     void deleteByStudentId(Long studentId);
 }
