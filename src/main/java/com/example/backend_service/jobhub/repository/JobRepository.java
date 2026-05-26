6a391396934fdb0997d99f8a74cb2ba9ef2cfaa7
Rakhitha Sooriyabandara <rakhitha.soori03@gmail.com>
2026-05-26 08:49:03 +0530

    reg bug fixings

--- src/main/java/com/example/backend_service/jobhub/repository/JobRepository.java
+++ src/main/java/com/example/backend_service/jobhub/repository/JobRepository.java
@@ -1,6 +1,7 @@
 package com.example.backend_service.jobhub.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,5 @@ import com.example.backend_service.jobhub.model.Job;
 public interface JobRepository extends JpaRepository<Job, Long> {
     List<Job> findByStatus(JobStatus status);
     List<Job> findByRecruiterId(Long recruiterId);
+    Optional<Job> findByIdAndRecruiterId(Long jobId, Long recruiterId);
 }
